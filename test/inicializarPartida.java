5afd19968d305fb0cca374aae163770648051888
Jorge <jmartinmen@cifpfbmoll.eu>
2021-02-05 12:45:03 +0100

    Primer intento de Inicializar partida

---
+++ test/inicializarPartida.java
@@ -0,0 +1,17 @@
+package test;
+
+import com.jaume.penjat.Tauler;
+
+public class inicializarPartida {
+    private Tauler tauler = new Tauler();
+    public void ParaulaSecreta(){
+        tauler.getParaulaSecreta();
+    }
+
+    public void NombreIntents(){
+        tauler.getIntents();
+    }
+
+
+
+}
