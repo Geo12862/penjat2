802b6b3bf7b0bca129debe08981ddf492e9ec814
Jorge <jmartinmen@cifpfbmoll.eu>
2021-02-05 16:14:30 +0100

    Rearmar Develope

---
+++ com/jaume/penjat/inicialitzarPartida.java
@@ -0,0 +1,13 @@
+package com.jaume.penjat;
+
+public class inicialitzarPartida {
+    private Tauler tauler = new Tauler();
+    public void ParaulaSecreta(){
+        tauler.getParaulaSecreta();
+    }
+
+    public void NombreIntents(){
+        tauler.getIntents();
+    }
+
+}
